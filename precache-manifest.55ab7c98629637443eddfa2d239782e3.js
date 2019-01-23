b59a20ce82e964948653d94d1b9c305ae938b395
Chris <christom525@yahoo.com>
2019-01-23 03:20:23 -0800

    Updates

---
+++ precache-manifest.55ab7c98629637443eddfa2d239782e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_Calculator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f5c8aaa85b1eb0dfbac3",
+    "url": "/React_Calculator/static/js/runtime~main.f5c8aaa8.js"
+  },
+  {
+    "revision": "e834429c5e60b92171d1",
+    "url": "/React_Calculator/static/js/main.e834429c.chunk.js"
+  },
+  {
+    "revision": "ea6ebb205542b9e55467",
+    "url": "/React_Calculator/static/js/1.ea6ebb20.chunk.js"
+  },
+  {
+    "revision": "e834429c5e60b92171d1",
+    "url": "/React_Calculator/static/css/main.1ca388f8.chunk.css"
+  },
+  {
+    "revision": "ea6ebb205542b9e55467",
+    "url": "/React_Calculator/static/css/1.307434ac.chunk.css"
+  },
+  {
+    "revision": "873420a5be659e533e900d133513dc33",
+    "url": "/React_Calculator/index.html"
+  }
+];
